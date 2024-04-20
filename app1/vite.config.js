510ab80fba3ba49ce11df553ce4d7c9ea9578f65
{tahireslam} <{tahir.eslam@gmail.com}>
2024-04-20 13:12:14 +0500

    removing vue

--- app1/vite.config.js
+++ app1/vite.config.js
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
-        vue,
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
